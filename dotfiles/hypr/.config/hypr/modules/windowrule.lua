930766f028d66d20704f7963a9aabac6eb2882b6
Ghostik <ghostik.tech@gmail.com>
2026-07-07 16:56:35 +0300

    feat(hypr): add window rules for swaync

--- dotfiles/hypr/.config/hypr/modules/windowrule.lua
+++ dotfiles/hypr/.config/hypr/modules/windowrule.lua
@@ -6,31 +6,31 @@
 -- uncomment all if you wish to use that.
 hl.workspace_rule({
 	workspace = "1",
-	animation = "slidevert"
+	animation = "slidevert",
 })
 hl.workspace_rule({
 	workspace = "2",
-	animation = "slidevert"
+	animation = "slidevert",
 })
 hl.workspace_rule({
 	workspace = "3",
-	animation = "slidevert"
+	animation = "slidevert",
 })
 hl.workspace_rule({
 	workspace = "4",
-	animation = "slidevert"
+	animation = "slidevert",
 })
 hl.workspace_rule({
 	workspace = "5",
-	animation = "slidevert"
+	animation = "slidevert",
 })
 hl.workspace_rule({
 	workspace = "6",
-	animation = "slidevert"
+	animation = "slidevert",
 })
 hl.workspace_rule({
 	workspace = "7",
-	animation = "slidevert"
+	animation = "slidevert",
 })
 
 -- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
@@ -54,10 +54,10 @@ local suppressMaximizeRule = hl.window_rule({
 	-- Ignore maximize requests from all apps. You'll probably like this.
 	name = "suppress-maximize-events",
 	match = {
-		class = ".*"
+		class = ".*",
 	},
 
-	suppress_event = "maximize"
+	suppress_event = "maximize",
 })
 -- suppressMaximizeRule:set_enabled(false)
 
@@ -70,10 +70,10 @@ hl.window_rule({
 		xwayland = true,
 		float = true,
 		fullscreen = false,
-		pin = false
+		pin = false,
 	},
 
-	no_focus = true
+	no_focus = true,
 })
 
 -- Layer rules also return a handle.
@@ -88,61 +88,61 @@ hl.window_rule({
 hl.window_rule({
 	name = "move-hyprland-run",
 	match = {
-		class = "hyprland-run"
+		class = "hyprland-run",
 	},
 
 	move = "20 monitor_h-120",
-	float = true
+	float = true,
 })
 
 hl.window_rule({
 	name = "float-telegram",
 	match = {
-		class = "org.telegram.desktop"
+		class = "org.telegram.desktop",
 	},
 
 	float = true,
 	center = true,
 	size = "700 900",
-	workspace = "unset"
+	workspace = "unset",
 })
 
 hl.window_rule({
 	name = "float-pavucontrol",
 	match = {
-		class = "org.pulseaudio.pavucontrol"
+		class = "org.pulseaudio.pavucontrol",
 	},
 
 	float = true,
 	center = true,
-	size = "700 500"
+	size = "700 500",
 })
 
 hl.window_rule({
 	name = "fix-android-studio-popups",
 	match = {
 		class = "jetbrains-studio",
-		title = "^win(.*)$"
+		title = "^win(.*)$",
 	},
 
-	no_initial_focus = true
+	no_initial_focus = true,
 })
 
 hl.window_rule({
 	name = "float-audiorelay",
 	match = {
 		class = "com-azefsw-audioconnect-desktop-app-MainKt",
-		title = "AudioRelay"
+		title = "AudioRelay",
 	},
 
 	float = true,
-	center = true
+	center = true,
 })
 
 hl.window_rule({
 	name = "flameshot-fix",
 	match = {
-		class = "flameshot"
+		class = "flameshot",
 	},
 
 	float = true,
@@ -150,15 +150,53 @@ hl.window_rule({
 
 	no_shadow = true,
 
-	workspace = "unset"
+	workspace = "unset",
 })
 
 -- Layer rules
 hl.layer_rule({
 	name = "blur-logout-dialog",
 	match = {
-		namespace = "logout_dialog"
+		namespace = "logout_dialog",
 	},
 
-	blur = true
+	blur = true,
+})
+
+hl.layer_rule({
+	name = "blur-swaync-control-center",
+	match = {
+		namespace = "swaync-control-center",
+	},
+
+	blur = true,
+})
+
+hl.layer_rule({
+	name = "alpha-swaync-control-center",
+	match = {
+		namespace = "swaync-control-center",
+	},
+
+	ignore_alpha = 0.48,
+})
+
+hl.layer_rule({
+	name = "blur-swaync-notification-window",
+
+	match = {
+		namespace = "swaync-notification-window",
+	},
+
+	blur = true,
+})
+
+hl.layer_rule({
+	name = "alpha-swaync-notification-window",
+
+	match = {
+		namespace = "swaync-notification-window",
+	},
+
+	ignore_alpha = 0.35,
 })
