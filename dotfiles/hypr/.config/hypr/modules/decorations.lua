c723d3aad8dff3fffd6dd73d8bfb089289207973
Ghostik <ghostik.tech@gmail.com>
2026-07-22 19:37:23 +0300

    feat(hypr): decrease windows In & Out style

--- dotfiles/hypr/.config/hypr/modules/decorations.lua
+++ dotfiles/hypr/.config/hypr/modules/decorations.lua
@@ -43,6 +43,7 @@ hl.config({
 			passes = 4,
 			vibrancy = 0.1696,
 			new_optimizations = true,
+      ignore_opacity = true
 		},
 	},
 
@@ -64,8 +65,8 @@ hl.curve("scrollSpring",   { type = "spring", mass = 0.8, stiffness = 110,     d
 hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" 													 })
 hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" 										 })
 hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, spring = "scrollSpring" 										 })
-hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" })
-hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
+hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 50%" })
+hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 50%" })
 hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" 										 })
 hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" 										 })
 hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" 														 })
